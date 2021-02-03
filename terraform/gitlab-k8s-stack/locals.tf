022809fa5c485a8158302f0f94a2f3f58ec7dbdf
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2021-02-03 01:25:58 +0100

    Fix hostname local variable

--- terraform/gitlab-k8s-stack/locals.tf
+++ terraform/gitlab-k8s-stack/locals.tf
@@ -1,5 +1,4 @@
 locals {
   name     = var.name == "" ? var.gitlab.ci_project_name : var.name
-  environment_hostname = var.gitlab.ci_environment_url != null ? replace(var.gitlab.ci_environment_url, "https://", "") : null
-  hostname = var.hostname != "" ? var.hostname : ( locals.environment_hostname )
+  hostname = var.hostname != "" ? var.hostname : ( var.gitlab.ci_environment_url != null ? replace(var.gitlab.ci_environment_url, "https://", "") : null )
 }
